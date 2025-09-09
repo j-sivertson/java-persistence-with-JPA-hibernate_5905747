c41ad5863ca8f56d49acb33a7a471866618e235a
Josh Sivertson <130604508+j-sivertson@users.noreply.github.com>
2025-09-09 19:53:25 +0000

    Update: Mapped Id's from my class entity to my database entity for books

--- src/main/java/com/mycompany/app/entities/Book.java
+++ src/main/java/com/mycompany/app/entities/Book.java
@@ -1,13 +1,20 @@
 package com.mycompany.app.entities;
 
+import javax.annotation.processing.Generated;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "book")
 public class Book {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "book_id")
   private int id;
 
